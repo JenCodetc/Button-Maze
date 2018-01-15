ea2844edb81d77418807a82327df342f84405e9d
Jennifer Li <brnli7@gmail.com>
2018-01-14 22:41:17 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,280 @@
+function reset() {
+    
+    for (var i=2; i<=50; i++){
+        document.getElementById(i +'').disabled=false;
+        document.getElementById(i +'').style.display="none";
+    }
+    
+    document.getElementById('1').disabled=false;
+}
+
+function one() {
+    document.getElementById('2').style.display='inline-block';
+    document.getElementById('3').style.display='inline-block';
+    
+//    document.getElementById('1').disabled=false;
+//    document.getElementById('2').disabled=false;
+//    document.getElementById('3').disabled=false;
+//    document.getElementById('5').disabled=false;
+//    document.getElementById('6').disabled=false;
+//    document.getElementById('7').disabled=false;
+//    document.getElementById('8').disabled=false;
+//    document.getElementById('9').disabled=false;
+//    document.getElementById('10').disabled=false;
+//    document.getElementById('11').disabled=false;
+//    document.getElementById('12').disabled=false;
+//    document.getElementById('13').disabled=false;
+//    document.getElementById('14').disabled=false;
+//    document.getElementById('15').disabled=false;
+//    document.getElementById('16').disabled=false;
+//    document.getElementById('19').disabled=false;
+//    document.getElementById('20').disabled=false;
+//    document.getElementById('22').disabled=false;
+//    document.getElementById('23').disabled=false;
+//    document.getElementById('24').disabled=false;
+//    document.getElementById('25').disabled=false;
+//    document.getElementById('26').disabled=false;
+//    document.getElementById('28').disabled=false;
+//    document.getElementById('29').disabled=false;
+//    document.getElementById('30').disabled=false;
+//    document.getElementById('31').disabled=false;
+//    document.getElementById('33').disabled=false;
+//    document.getElementById('35').disabled=false;
+//    document.getElementById('36').disabled=false;
+//    document.getElementById('37').disabled=false;
+//    document.getElementById('38').disabled=false;
+//    document.getElementById('39').disabled=false;
+//    document.getElementById('40').disabled=false;
+//    document.getElementById('41').disabled=false;
+//    document.getElementById('42').disabled=false;
+}
+
+function two() {
+    document.getElementById('4').style.display='inline-block';
+    
+    document.getElementById('3').disabled=true;
+}
+
+function three() {
+    document.getElementById('5').style.display='inline-block';
+    document.getElementById('6').style.display='inline-block';
+    
+
+    document.getElementById('2').disabled=true;
+}
+
+function five() {
+    document.getElementById('7').style.display='inline-block';
+    document.getElementById('8').style.display='inline-block';
+    
+    document.getElementById('6').disabled=true;
+}
+
+function six() {
+    document.getElementById('9').style.display='inline-block';
+    document.getElementById('10').style.display='inline-block';
+    
+    document.getElementById('5').disabled=true;
+}
+
+function seven() {
+    document.getElementById('11').style.display='inline-block';
+    document.getElementById('12').style.display='inline-block';
+    
+    document.getElementById('8').disabled=true;
+}
+
+function eight() {
+    document.getElementById('13').style.display='inline-block';
+    document.getElementById('14').style.display='inline-block';
+    
+    document.getElementById('7').disabled=true;
+}
+
+function nine() {
+    document.getElementById('15').style.display='inline-block';
+    document.getElementById('16').style.display='inline-block';
+    
+    document.getElementById('10').disabled=true;
+}
+
+function ten() {
+    document.getElementById('17').style.display='inline-block';
+    
+    document.getElementById('9').disabled=true;
+}
+
+function eleven() {
+    document.getElementById('18').style.display='inline-block';
+    
+    document.getElementById('12').disabled=true;
+}
+
+function twelve() {
+    document.getElementById('19').style.display='inline-block';
+    document.getElementById('20').style.display='inline-block';
+    
+    document.getElementById('11').disabled=true;
+}
+
+function thirteen() {
+    document.getElementById('21').style.display='inline-block';
+    
+    document.getElementById('14').disabled=true;
+}
+
+function fourteen() {
+    document.getElementById('20').style.display='inline-block';
+    document.getElementById('22').style.display='inline-block';
+    
+    document.getElementById('13').disabled=true;
+}
+
+function fifteen() {
+    document.getElementById('22').style.display='inline-block';
+    document.getElementById('23').style.display='inline-block';
+    
+    document.getElementById('16').disabled=true;
+}
+
+function sixteen() {
+    document.getElementById('23').style.display='inline-block';
+    document.getElementById('24').style.display='inline-block';
+    
+    document.getElementById('15').disabled=true;
+}
+
+function nineteen() {
+    document.getElementById('25').style.display='inline-block';
+    document.getElementById('26').style.display='inline-block';
+    
+    document.getElementById('20').disabled=true;
+}
+
+function twenty() {
+    document.getElementById('27').style.display='inline-block';
+    
+    document.getElementById('19').disabled=true;
+    document.getElementById('22').disabled=true;
+}
+
+function twentytwo() {
+    document.getElementById('28').style.display='inline-block';
+    document.getElementById('29').style.display='inline-block';
+    
+    document.getElementById('22').disabled=true;
+    document.getElementById('23').disabled=true;
+}
+
+function twentythree() {
+    document.getElementById('30').style.display='inline-block';
+    document.getElementById('31').style.display='inline-block';
+    
+    document.getElementById('24').disabled=true;
+}
+
+function twentyfour() {
+    document.getElementById('32').style.display='inline-block';
+    
+    document.getElementById('23').disabled=true;
+}
+
+function twentyfive() {
+    document.getElementById('33').style.display='inline-block';
+    document.getElementById('34').style.display='inline-block';
+    
+    document.getElementById('26').disabled=true;
+}
+
+function twentysix() {
+    document.getElementById('35').style.display='inline-block';
+    document.getElementById('36').style.display='inline-block';
+    
+    document.getElementById('25').disabled=true;
+}
+
+function twentyeight() {
+    document.getElementById('37').style.display='inline-block';
+    document.getElementById('38').style.display='inline-block';
+    
+    document.getElementById('29').disabled=true;
+}
+
+function twentynine() {
+    document.getElementById('39').style.display='inline-block';
+    
+    document.getElementById('28').disabled=true;
+}
+
+function thirty() {
+    document.getElementById('40').style.display='inline-block';
+    
+    document.getElementById('31').disabled=true;
+}
+
+function thirtyone() {
+    document.getElementById('41').style.display='inline-block';
+    document.getElementById('42').style.display='inline-block';
+    
+    document.getElementById('30').disabled=true;
+}
+
+function thirtythree() {
+    document.getElementById('43').style.display='inline-block';
+    
+    document.getElementById('34').disabled=true;
+}
+
+function thirtyfour() {
+    document.getElementById('44').style.display='inline-block';
+    
+    document.getElementById('33').disabled=true;
+}
+
+function thirtyfive() {
+    document.getElementById('45').style.display='inline-block';
+    
+    document.getElementById('36').disabled=true;
+}
+
+function thirtysix() {
+    document.getElementById('46').style.display='inline-block';
+    
+    document.getElementById('35').disabled=true;
+}
+
+function thirtyseven() {
+    document.getElementById('47').style.display='inline-block';
+    
+    document.getElementById('38').disabled=true;
+}
+
+function thirtyeight() {
+    document.getElementById('48').style.display='inline-block';
+    
+    document.getElementById('37').disabled=true;
+    
+    alert("Congrats! You beat the maze!!");
+}
+
+function fortyone() {
+    document.getElementById('49').style.display='inline-block';
+    
+    document.getElementById('42').disabled=true;
+}
+
+function fortytwo() {
+    document.getElementById('50').style.display='inline-block';
+    
+    document.getElementById('41').disabled=true;
+}
+
+//      var count = 1;
+//      var button = document.getElementById("countButton");
+//      var display = document.getElementById("displayCount");
+//
+//      button.onclick = function(){
+//        count++;
+//        display.innerHTML = count;
+//      }
+
